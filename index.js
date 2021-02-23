a22d818c426d0a315c70b844b32041dca1d4d5f0
Victor Nwani <itsvictornwani@yahoo.com>
2021-02-23 14:13:11 -0500

    completed

--- index.js
+++ index.js
@@ -1,17 +1,54 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function (a, b) {
+      return (a - b);
+    })
+    this.length = this.items.length;
 
-  get(pos) {}
+  }
 
-  max() {}
+  get(pos) {
+    return this.items[pos - 1];
+  }
 
-  min() {}
+  max() {
+    if (this.items.length > 0) {
+      return Math.max.apply(null, this.items);
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
 
-  sum() {}
+  min() {
+    if (this.items.length > 0) {
+      return Math.min.apply(null, this.items);
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
 
-  avg() {}
-}
+  average() {
+    if (this.items.length > 0) {
+      return this.sum() / this.items.length;
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
+  sum() {
+    if (this.items.length > 0) {
+      return this.items.reduce(function (a, b) {
+        return a + b;
+      })
+    } else {
+      return 0;
+    }
+  }
+};
 
 module.exports = SortedList;
